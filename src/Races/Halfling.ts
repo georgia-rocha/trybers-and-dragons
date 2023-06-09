e1019f1c50bb45e5f5eb80f3addf560eb01ac38f
Georgia <georgiab543@gmail.com>
2023-06-08 22:43:21 -0300

    feat: requisito 2 - cria classes que herdam de Race

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,5 +1,23 @@
 import Race from './Race';
 
 export default class Halfling extends Race {
+  private static _createdInstances = 0;
+  private _maxLifePoints: number;
 
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    this._maxLifePoints = 60;
+    Halfling._createdInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static override createdRacesInstances(): number {
+    return Halfling._createdInstances;
+  }
 }
---
+++ src/Races/Orc.ts
@@ -0,0 +1,23 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  private static _createdInstances = 0;
+  private _maxLifePoints: number;
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc._createdInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static override createdRacesInstances(): number {
+    return Orc._createdInstances;
+  }
+}
